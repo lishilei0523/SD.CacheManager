93f2a2b6675f6ca956798d5b1d5884dff035b7d1
Lee <lishilei0523@163.com>
2016-07-25 14:42:33 +0800

    整理Memcached缓存实现，并添加测试

--- SD.CacheManager.MemcachedTests/CacheMediatorTests.cs
+++ SD.CacheManager.MemcachedTests/CacheMediatorTests.cs
@@ -1,14 +1,87 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SD.CacheManager.Mediator;
 
 namespace SD.CacheManager.MemcachedTests
 {
+    /// <summary>
+    /// 缓存测试
+    /// </summary>
     [TestClass]
     public class CacheMediatorTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        /// <summary>
+        /// 初始化测试
+        /// </summary>
+        [TestInitialize]
+        public void Init()
         {
+            //清空缓存
+            CacheMediator.Clear();
+        }
+
+        /// <summary>
+        /// 测试插入与读取缓存
+        /// </summary>
+        [TestMethod]
+        public void TestSetAndGetCache()
+        {
+            string cacheKey = "key";
+            string cacheValue = "value";
+
+            CacheMediator.Set(cacheKey, cacheValue);
+
+            string value = CacheMediator.Get<string>(cacheKey);
+            Assert.IsTrue(value == cacheValue);
+        }
+
+        /// <summary>
+        /// 测试移除缓存
+        /// </summary>
+        [TestMethod]
+        public void TestRemoveCache()
+        {
+            string cacheKey = "key";
+            string cacheValue = "value";
+
+            CacheMediator.Set(cacheKey, cacheValue);
+
+            //移除
+            CacheMediator.Remove(cacheKey);
+
+            string value = CacheMediator.Get<string>(cacheKey);
+            Assert.IsNull(value);
+        }
+
+        /// <summary>
+        /// 测试清空缓存
+        /// </summary>
+        [TestMethod]
+        public void TestClearCache()
+        {
+            string cacheKey1 = "key1";
+            string cacheKey2 = "key2";
+            string cacheValue1 = "value1";
+            string cacheValue2 = "value2";
+
+            CacheMediator.Set(cacheKey1, cacheValue1);
+            CacheMediator.Set(cacheKey2, cacheValue2);
+
+            //移除全部
+            CacheMediator.Clear();
+
+            string value1 = CacheMediator.Get<string>(cacheKey1);
+            string value2 = CacheMediator.Get<string>(cacheKey2);
+
+            Assert.IsTrue(value1 == null && value2 == null);
+        }
+
+        /// <summary>
+        /// 清理
+        /// </summary>
+        [TestCleanup]
+        public void TestFinalize()
+        {
+            CacheMediator.Clear();
         }
     }
 }
