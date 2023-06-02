24c4be562a3e92602f4c5f6b7863126771beb63b
Lee <lishilei0523@163.com>
2023-06-02 10:40:45 +0800

    优化警告；

--- src/SD.CacheManager/Providers/NullProvider.cs
+++ src/SD.CacheManager/Providers/NullProvider.cs
@@ -9,12 +9,40 @@ namespace SD.CacheManager
     /// </summary>
     public class NullProvider : ICacheAdapter
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public void Set<T>(string key, T value) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void Set<T>(string key, T value, DateTime expiredTime) { }
-        public T Get<T>(string key) { return default(T); }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public T Get<T>(string key) { return default; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void Remove(string key) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void RemoveRange(IEnumerable<string> keys) { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public bool Exists(string key) { return false; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void Dispose() { }
     }
 }
